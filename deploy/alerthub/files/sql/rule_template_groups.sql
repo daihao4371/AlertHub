4b9721712ac26a6a24224626310948165800bcf8
daihao4371 <daihao_4371@163.com>
2026-01-27 11:47:45 +0800

    refactor: 将 SQL 文件中的数据库名从 watchalert 重命名为 alerthub

--- deploy/alerthub/files/sql/rule_template_groups.sql
+++ deploy/alerthub/files/sql/rule_template_groups.sql
@@ -1,5 +1,5 @@
 use watchalert;
-INSERT ignore INTO watchalert.rule_template_groups (name,`number`,description,`type`) VALUES
+INSERT ignore INTO alerthub.rule_template_groups (name,`number`,description,`type`) VALUES
 	 ('APISIX',0,'APISIX指标监控','Metrics'),
 	 ('服务日志监控',0,'服务日志监控','Logs'),
 	 ('Docker',0,'Docker容器监控','Metrics'),
@@ -10,7 +10,7 @@ INSERT ignore INTO watchalert.rule_template_groups (name,`number`,description,`t
 	 ('Kubernetes',0,'Kubernetes事件监控','Events'),
 	 ('KubernetesMetric',0,'Kubernetes指标监控','Metrics'),
 	 ('MongoDB',0,'MongoDB监控','Metrics');
-INSERT ignore INTO watchalert.rule_template_groups (name,`number`,description,`type`) VALUES
+INSERT ignore INTO alerthub.rule_template_groups (name,`number`,description,`type`) VALUES
 	 ('MySQL',0,'MySQL资源监控','Metrics'),
 	 ('Node节点监控',0,'Node服务器监控','Metrics'),
 	 ('Redis',0,'Redis资源监控','Metrics'),
